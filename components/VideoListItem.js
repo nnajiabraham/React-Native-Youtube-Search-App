18864c1ad4df40e09639ab83c76494b66522d951
nnajiabraham <nnajiabraham@gmail.com>
2018-09-17 18:50:51 -0500

    added cards UI

--- components/VideoListItem.js
+++ components/VideoListItem.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { Card } from 'react-native-elements';
 
 const VideoListItem = ({ video }) => {
-  const { imageStyle } = styles;
+  const {
+    imageStyle,
+    cardStyle,
+    contentStyle,
+    titleStyle,
+    channelTitleStyle,
+    descriptionStyle
+  } = styles;
+
   const {
     title,
     channelTitle,
@@ -14,15 +23,40 @@ const VideoListItem = ({ video }) => {
 
   return (
     <View>
-      <Image style={imageStyle} source={{ uri: url }} />
-      <Text>{title}</Text>
-      <Text>{channelTitle}</Text>
-      <Text>{description}</Text>
+      <Card containerStyle={cardStyle}>
+        <Image style={imageStyle} source={{ uri: url }} />
+        <View style={contentStyle}>
+          <Text style={titleStyle}>{title}</Text>
+          <Text style={channelTitleStyle}>{channelTitle}</Text>
+          <Text style={descriptionStyle}>{description}</Text>
+        </View>
+      </Card>
     </View>
   );
 };
 
 const styles = {
+  cardStyle: {
+    padding: 5
+  },
+  contentStyle: {
+    flex: 1,
+    padding: 5
+  },
+  titleStyle: {
+    fontSize: 12,
+    marginBottom: 5
+  },
+  channelTitleStyle: {
+    fontSize: 11,
+    color: '#777',
+    marginBottom: 5,
+    alignSelf: 'flex-end'
+  },
+  descriptionStyle: {
+    fontSize: 11,
+    alignSelf: 'center'
+  },
   imageStyle: {
     alignSelf: 'stretch',
     height: 280
