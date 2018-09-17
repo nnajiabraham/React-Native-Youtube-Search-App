cd0517b57996c172750efa45fc91d9e88aa6adb0
nnajiabraham <nnajiabraham@gmail.com>
2018-09-17 18:23:26 -0500

    Integrate Youtube API

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Header } from 'react-native-elements';
+import YTSearch from 'youtube-api-search';
+import AppHeader from './components/Header';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+
+const API_KEY = 'AIzaSyDYQa2SclhYcGI55z8iEUxEydh0bPJB-5M';
 
 export default class App extends Component {
+  state = {
+    loading: false,
+    videos: []
+  };
+
+  onPressSearch = term => {
+    this.searchYT(term);
+  };
+
+  searchYT = term => {
+    this.setState({ loading: true });
+    YTSearch({ key: API_KEY, term }, videos => {
+      this.setState({
+        loading: false,
+        videos
+      });
+    });
+  };
+
   render() {
+    const { loading, videos } = this.state;
+
     return (
-      <View style={{ flex: 1, backgroundColor: '#333' }}>
-        <Header
-          centerComponent={{ text: 'React Native Youtube Search App', style: { color: 'white' } }}
-          outerContainerStyles={{ backgroundColor: '#E91431' }}
-        />
-        {/**/}
-        {/**/}
+      <View style={{ flex: 1, backgroundColor: '#ddd' }}>
+        <AppHeader headerText="React-Native YouTube Search" />
+        <SearchBar loading={loading} onPressSearch={this.onPressSearch} />
+        <VideoList videos={videos} />
       </View>
     );
   }
